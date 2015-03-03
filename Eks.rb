2c7a0c780d3645b3da513e8f093b7ff723314824
George Pimm <jorjpimm@gmail.com>
2015-03-03 18:43:16 +0000

    adding new build system

---
+++ Eks.rb
@@ -0,0 +1,23 @@
+require 'modules/global'
+
+RbMake.module(:Eks, :global) do |l, p|
+  l.extend_with(:cpp)
+  l.cpp.flags = [ :warn_as_error, :cpp14, '-stdlib=libc++' ]
+
+  l.cpp.defines = [ :X_CPPOX_SUPPORT, l.name.to_s.upcase + "_BUILD" ]
+  l.when(l.config.debug) do |l|
+    l.cpp.defines << :X_DEBUG
+  end
+
+  l.dependencies << :eigen
+
+  l.when(l.config.platform(:osx)) do |l|
+    l.cpp.minimum_osx_version = "10.9"
+  end
+
+  l.export do |l|
+    l.include_paths << 'include'
+  end
+end
+
+RbMake.import_modules('Eks*')
